74a0e77b69dab750c5f1e9235efa8a7e8362a7a1
Buergluk <lukas@buerglers.ch>
2024-12-24 10:44:47 +0100

    .

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,5 @@
-<h1>Contact</h1>
+<h1>Kontakt</h1>
+<p>Wir freuen uns, von dir zu hören! Ob du Fragen zu unseren Berghütten, zur Buchung oder zu besonderen Wünschen hast – wir sind hier, um dir zu helfen.</p>
+<h2>So erreichst du uns:</h2>
+<p> 📧 E-Mail: info@example.ch
+    📞 Telefon: +49 123 456 7890</p>
