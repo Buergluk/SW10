8b6034de0c3c345d94d96098690e884645f6b0d4
Buergluk <lukas@buerglers.ch>
2024-12-24 11:20:02 +0100

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 </script>
 
 <nav>
+    <img src="/src/routes/Logo.png" alt="Logo" class="logo">
     <a href="/">Home</a>
     <a href="/contact">Contact</a>
     <a href="/mountainhuts">Mountainhuts</a>
